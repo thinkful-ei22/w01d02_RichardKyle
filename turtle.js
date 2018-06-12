16e19ac0b52284a5b7306635070346429855e8d6
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-06-12 12:34:03 -0700

    Clean up commenting and organization in turtle.js

--- turtle.js
+++ turtle.js
@@ -2,18 +2,21 @@
 
 const turtleMovements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
 
+// Version 1:
 // Filter
-const filteredMovements = turtleMovements.filter(move => (move[0] >= 0 && move[1] >= 0));
+// const filteredMovements = turtleMovements.filter(move => (move[0] >= 0 && move[1] >= 0));
 // console.log(filteredMovements);
 
 // Map
-const numSteps = filteredMovements.map(move => move[0] + move[1]);
+// const numSteps = filteredMovements.map(move => move[0] + move[1]);
 // console.log(numSteps);
 
 // forEach
 // numSteps.forEach(num => console.log(`The turtle took ${num} steps.`));
 
+// Version 2: (refactored)
 turtleMovements
   .filter(move => (move[0] >= 0 && move[1] >= 0))
   .map(move => move[0] + move[1])
   .forEach(num => console.log(`The turtle took ${num} steps.`));
+  
